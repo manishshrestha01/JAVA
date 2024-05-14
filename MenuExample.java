9087ad72261934e8b17d3954c273a76e06d0dab7
manishstha27 <shresthamanish8080@gmail.com>
2024-05-14 11:19:31 +0545

    first

---
+++ MenuExample.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuExample {
+public static void main(String[] args) {
+    JFrame frame = new JFrame("Menu Example");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400,300);
+
+    JMenuBar menuBar = new JMenuBar();
+    JMenu fileMenu = 
+}
+    
+}
